7d6ad82a1893890f5a06da33bc123e46268fdcb1
Ahmedelsayed55 <am1096922@gmail.com>
2025-10-24 20:26:04 +0300

    vv

--- src/Componants/Information.jsx
+++ src/Componants/Information.jsx
@@ -56,7 +56,7 @@ export const Informaition = () => {
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
           <motion.div
-            initial={{ opacity: 0, x: -230 }}
+            initial={{ opacity: 0, x: -150 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.1, duration: 1.5 }}
             viewport={{ once: false, amount: 0.3 }}
@@ -66,7 +66,7 @@ export const Informaition = () => {
             <p className="text-3xl">18°</p>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, x: -230 }}
+            initial={{ opacity: 0, x: -150 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.1, duration: 1 }}
             viewport={{ once: false, amount: 0.3 }}
@@ -76,7 +76,7 @@ export const Informaition = () => {
             <p className="text-3xl">46%</p>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, x: -230 }}
+            initial={{ opacity: 0, x: -150 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.1, duration: 0.5 }}
             viewport={{ once: false, amount: 0.3 }}
@@ -86,7 +86,7 @@ export const Informaition = () => {
             <p className="text-3xl">14 km/h</p>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, x: -230 }}
+            initial={{ opacity: 0, x: -150 }}
            whileInView={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.1, duration: 0.1 }}
             viewport={{ once: false, amount: 0.3 }}
